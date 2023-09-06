3bdcc732b9559f738c2ab944fd1f0020e57442c4
Jonathan Hong <143454662+Soup1ette@users.noreply.github.com>
2023-09-06 14:22:12 -0700

    Update git-practice.py

--- git-practice.py
+++ git-practice.py
@@ -1,5 +1,11 @@
-first = "Jonathan"
-middle = "Y"
-last = "Hong"
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
 
-print("My name is", first, middle, last)
+def addItem(item):
+   # Finish this code
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
