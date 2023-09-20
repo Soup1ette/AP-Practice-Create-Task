8b3d1fd33ac7d915b50620f8da27de73e5acd5cd
Jonathan Hong <jonnyhongy@gmail.com>
2023-09-20 21:35:21 +0000

    Finished comments and stuff.

--- actual-practice-create-task.py
+++ actual-practice-create-task.py
@@ -1,3 +1,5 @@
+#This code counts the amounts of A, P, or R inputted by the user, which represents aluminum, plastic, or paper trash thrown in a trash bin that can identify their types. However, if the trash thrown in the bin isn't aluminum, paper, or plastic, the trash bin starts melting.#
+
 total_count = {"aluminum": 0, "plastic": 0, "paper": 0}
 
 sortItems = input("What has been collected today? (A, P, or R) ")
@@ -11,6 +13,7 @@ def give(sortItems):
 count_A = sortItems.count('A')
 count_P = sortItems.count('P')
 count_R = sortItems.count('R')
+#The function is being called on the line under here: Line 17.#
 horse_meat = give(sortItems)
 
 total_count["aluminum"] = total_count["aluminum"] + count_A
